b6270efc4cf18bcfddcebaea602171c7b79d9088
Prashant Shivaji Jadhav <125643307+Prashantjadhav2620@users.noreply.github.com>
2023-12-25 10:12:09 +0530

    add server file

---
+++ server/DAL/Services/Interface/ILocationServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+namespace DAL.Services.Interface
+{
+    public interface ILocationServices
+    {
+        Task<string> CreateLocation(Location s);
+        Task<string> UpdateLocation(Location s);
+        Task<string> DeleteLocation(int id);
+        Task<string> RestoreLocation(int id);
+        Task<List<Location>> GetAllLocation();
+        Task<Location> GetLocationById(int Location_id);
+        Task<Location> GetAllLocationById(int Location_id);
+    }
+}
---
+++ server/DAL/Services/Interface/IStateServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+
+namespace DAL.Services.Interface
+{
+    public interface IStateServices
+    {
+        Task<string> CreateState(State s);
+        Task<string> UpdateState(State s);
+        Task<string> DeleteState(int id);
+        Task<string> RestoreState(int id);
+        Task<List<State>> GetAllStates();
+        Task<State> GetStateById(int state_id);
+    }
+}
